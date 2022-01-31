089d2edcf2e7aa2b14087348115c3940fa272e95
Roylam23 <roylam23@gmail.com>
2022-02-01 04:14:41 +0800

    v0.3.6

---
+++ next-sitemap.js
@@ -0,0 +1,4 @@
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://jfft.vercel.app/',
+    generateRobotsTxt: true,
+}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   reactStrictMode: true,
+  siteUrl: process.env.SITE_URL || 'https://jfft.vercel.app',
+  generateRobotsTxt: true, 
 }

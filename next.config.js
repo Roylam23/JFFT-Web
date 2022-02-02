5b8bfef6ed7624d7c6b81d20d2dd37c4054ab205
Roylam23 <roylam23@gmail.com>
2022-02-03 00:36:46 +0800

    v0.7

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   reactStrictMode: true,
   siteUrl: process.env.SITE_URL || 'https://jfft.vercel.app',
-  generateRobotsTxt: true, 
-}
+  generateRobotsTxt: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});

b659fd029a91c86f11e01f8278240d6d1d55ffc8
Roylam23 <roylam23@gmail.com>
2022-02-03 04:13:47 +0800

    v0.7.3

--- pages/index.js
+++ pages/index.js
@@ -371,16 +371,6 @@ export default function Home({ data }) {
     };
 
     window.addEventListener("scroll", handleScroll);
-    let installPromptEvent;
-
-    window.addEventListener("beforeinstallprompt", (event) => {
-      // Prevent Chrome <= 67 from automatically showing the prompt
-      event.preventDefault();
-      // Stash the event so it can be triggered later.
-      installPromptEvent = event;
-      // Update the install UI to notify the user app can be installed
-      document.querySelector("#install-button").disabled = false;
-    });
   }, []);
   const height = use100vh();
 

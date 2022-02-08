e5d86e551fb2dd809bc5fb7523d32b171838e526
Roylam23 <roylam23@gmail.com>
2022-02-09 04:43:11 +0800

    instagram bug v0.2

--- public/js/instagram.js
+++ public/js/instagram.js
@@ -1,9 +1,10 @@
 
 var ua = navigator.userAgent || navigator.vendor || window.opera;
-var isInstagram = (ua.indexOf('Instagram') > -1) ? true : false;
+var isSafari = (ua.indexOf('Safari') > -1) ? true : false;
+var isWebkit = (ua.indexOf('Webkit') > -1) ? true : false;
 
 if (document.documentElement.classList ){
-	if (isInstagram) {
+	if (isWebkit && !isSafari) {
 		alert("建議使用Safari, Chrome等瀏覽器瀏覽以達致最佳觀賞效果")
 	}
 }

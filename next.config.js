7e7bfbacc05d97c5f535c586315ee18539e9f057
Roylam23 <roylam23@gmail.com>
2022-02-08 21:42:05 +0800

    navbar transparent

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 const withPWA = require("next-pwa");
+const prod = process.env.NODE_ENV === "production";
 
 module.exports = withPWA({
   reactStrictMode: true,
-  siteUrl: process.env.SITE_URL || 'https://jfft.vercel.app',
+  siteUrl: process.env.SITE_URL || "https://jfft.vercel.app",
   generateRobotsTxt: true,
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
+    disable: prod ? false : true,
   },
 });

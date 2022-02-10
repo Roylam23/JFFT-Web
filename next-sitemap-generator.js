b3c34adba79fff9299276691253de397acfbf647
Roylam23 <roylam23@gmail.com>
2022-02-10 09:00:37 +0800

    v1.7.5

--- next-sitemap-generator.js
+++ next-sitemap-generator.js
@@ -1,18 +1,18 @@
-const sitemap =  require("nextjs-sitemap-generator");
+const sitemap = require("nextjs-sitemap-generator");
 
-const fs =  require("fs");
+const fs = require("fs");
 
-  
+
 
 const BUILD_ID = fs.readFileSync(".next/BUILD_ID").toString();
 
-  
+
 
 sitemap({
-  baseUrl: "https://jfft.pages.dev",
-  // If you are using Vercel platform to deploy change the route to /.next/serverless/pages 
-  pagesDirectory: __dirname +"/pages",
-  targetDirectory: "public/",
-  ignoredExtensions: ["map"],
-  ignoredPaths: ["assets"], // Exclude everything that isn't static page
+    baseUrl: "https://jfft.pages.dev",
+    // If you are using Vercel platform to deploy change the route to /.next/serverless/pages 
+    pagesDirectory: __dirname + "/pages",
+    targetDirectory: "public/",
+    ignoredExtensions: ["map"],
+    ignoredPaths: ["assets", "components", "api"], // Exclude everything that isn't static page
 });

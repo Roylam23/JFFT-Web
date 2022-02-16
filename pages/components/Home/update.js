980bdb6f52e4fb2220be5e7a24ee189b8ebb2af1
Roylam23 <roylam23@gmail.com>
2022-02-16 14:52:10 +0800

    v1.0 Stable

--- pages/components/Home/update.js
+++ pages/components/Home/update.js
@@ -5,7 +5,7 @@ import styles from "../../../styles/Home.module.css";
 const Update = () => {
   return (
     <motion.div
-      className={styles.textCon}
+      className={styles.updateCon}
       id={styles.textCon}
       style={{ height: "auto" }}
     >
@@ -13,11 +13,11 @@ const Update = () => {
       <div className={styles.frame} id={styles.frame}></div>
       <motion.div
         className={styles.text2}
-        initial={{ y: 20, opacity: 0 }}
+        initial={{ y: 25, opacity: 0 }}
         whileInView={{
           y: 0,
           opacity: 1,
-          transition: { duration: .5 },
+          transition: { duration: .75 },
         }}
         viewport={{ margin: "-20% 0%" }}
         style={{
@@ -27,7 +27,7 @@ const Update = () => {
         <span>網站初期設計少柒敬請見諒</span>
         <span>內容同功能會因功課進度而更新</span>
         <span>加入JFFT永遠都唔會遲！</span>
-        <span>在此感謝JFFT為各位提供歡樂</span>
+        <span>感謝JFFT為各位會然帶黎歡樂</span>
       </motion.div>
     </motion.div>
   );
